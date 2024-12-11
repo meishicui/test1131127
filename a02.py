5c5e8b0b6b1639221d534296a1201e121cb93cbd
meishicui <166352454+meishicui@users.noreply.github.com>
2024-12-11 16:43:21 +0800

    Add files via upload

---
+++ a02.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/abc')
+def abc():
+    return 'abcHello, World'
+if __name__ =="__main__":
+    app.run()
